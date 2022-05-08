9b1cfff46820a63380307620d802fb9732010178
Anurag Akkiraju <anurag.akkiraju@gmail.com>
2022-05-08 00:03:59 -0400

    init

---
+++ IsRotation.java
@@ -0,0 +1,6 @@
+public class IsRotation {
+	boolean is_rotation(String s1, String s2){
+		
+		return false;
+	}	
+}
---
+++ RemoveDuplicates.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+// LinkedListNode is defined in book (alternately):
+// https://github.com/careercup/CtCI-6th-Edition/blob/59018cfcb90292209275db1c4b3ed306d4b07d7f/Java/CtCILibrary/CtCILibrary/LinkedListNode.java
+public class Remove_Duplicates{
+	// key here is to use a hashset, as lookup is 0(1) complexity
+
+	void remove_duplicates(LinkedListNode n){
+		HashSet<Integer> set = new HashSet<Integer>();
+
+		LinkedListNode prev = null;
+
+		while(n != null){
+			if(set.contains(n)){
+				set.remove(n);
+			}
+			else{
+				set.add(n);
+				prev = n;
+			}
+			n = n.next;
+		}
+	}
+}
